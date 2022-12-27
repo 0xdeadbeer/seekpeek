2fb5372596c7a6b97f40c18a8f24fa7fc4a93e69
osamu-kj <kevin.jerebica@gmail.com>
2022-12-27 22:34:22 +0100

    General: Add a simple searchbar

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-GTK_FLAGS=`pkg-config --cflags gtk+-3.0`
-GTK_LIBS=`pkg-config --libs gtk+-3.0`
+GTK_FLAGS=`pkg-config --cflags gtk4`
+GTK_LIBS=`pkg-config --libs gtk4`
 SP_OUTPUT_DIR=build
 SP_OUTPUT_NAME=seekpeek
 SP_OUTPUT_LOCATION=$(SP_OUTPUT_DIR)/$(SP_OUTPUT_NAME)
@@ -7,6 +7,14 @@ SP_OUTPUT_LOCATION=$(SP_OUTPUT_DIR)/$(SP_OUTPUT_NAME)
 run: 
 	./$(SP_OUTPUT_LOCATION)
 
-all: 
+build: 
+	mkdir -p $(SP_OUTPUT_DIR)
 	gcc $(GTK_FLAGS) main.c -o $(SP_OUTPUT_LOCATION) $(GTK_LIBS)
+
+clean: 
+	rm -rf $(SP_OUTPUT_DIR)/
+
+all: 
+	make clean
+	make build 
 	make run 
--- main.c
+++ main.c
@@ -1,24 +1,56 @@
 #include <gtk/gtk.h>
 
-static void activate(GtkApplication *app, gpointer user_data) {
-    GtkWidget *window; 
+static GtkWidget *window; 
+static GtkCssProvider *provider; 
+static GdkDisplay *display; 
 
-    window = gtk_application_window_new(app); 
-    gtk_window_set_title(GTK_WINDOW(window), "window"); 
-    gtk_window_set_default_size(GTK_WINDOW (window), 200, 200); 
+static void activate (GtkApplication *app, gpointer user_data)
+{
+  GtkWidget *grid; 
+  GtkWidget *button; 
+  GtkWidget *text;
 
-    gtk_window_present(GTK_WINDOW(window));
+  grid = gtk_grid_new(); 
+  gtk_window_set_child(GTK_WINDOW(window), grid);
+  gtk_widget_add_css_class(grid, "searchbar");
+
+  text = gtk_text_new(); 
+  gtk_grid_attach(GTK_GRID(grid), text, 0, 0, 1, 1); 
+  gtk_widget_add_css_class(text, "search-input");
+  gtk_text_set_placeholder_text(text, "query"); 
+
+  button = gtk_button_new_with_label("Search!");
+  gtk_grid_attach(GTK_GRID(grid), button, 1, 0, 1, 1); 
+  gtk_widget_add_css_class(button, "search-button"); 
+
+  gtk_window_present (GTK_WINDOW (window));
 }
 
-int main (int argc, char **argv) {
-    GtkApplication *app; 
-    int status; 
+static void startup(GApplication *application) {
+  GtkApplication *app = GTK_APPLICATION(application); 
+  
+  // setup window 
+  window = gtk_application_window_new(app); 
+  gtk_window_set_title(GTK_WINDOW(window), "Seekpeek"); 
+  gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
 
-    app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE); 
-    g_signal_connect(app, "activate", G_CALLBACK (activate), NULL); 
-    status = g_application_run(G_APPLICATION(app), argc, argv); 
+  // setup display
+  display = gtk_widget_get_display(GTK_WIDGET(window)); 
+  provider = gtk_css_provider_new(); 
+  gtk_css_provider_load_from_file(provider, g_file_new_for_path("/home/unknown/projects/seekpeek/style/main.css"));
+  gtk_style_context_add_provider_for_display(display, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+}
 
-    g_object_unref(app); 
+int main (int argc, char **argv)
+{
+  GtkApplication *app;
+  int status;
 
-    return status; 
+  app = gtk_application_new ("org.osamu-sp.seekpeek", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect(app, "startup", G_CALLBACK(startup), NULL);
+  g_signal_connect(app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
 }
